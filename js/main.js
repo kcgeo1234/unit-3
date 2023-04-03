715aadb04fa9f1f02630d4855ce33be6e739ca74
kcgeo1234 <kcheng38@wisc.edu>
2023-04-03 12:17:21 -0500

    Activity 9

--- js/main.js
+++ js/main.js
@@ -1,158 +1,89 @@
 // Add all scripts to the JS folder
-//SVG dimension variables
-var w = 900, h = 500;
+//begin script when window loads
+window.onload = setMap();
 
-//Example 1.2 line 1...container block
-window.onload = function(){
-    var cityPop = [
-        { 
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
+//set up choropleth map
+function setMap(){
+    //map frame dimensions
+    var width = 960, height = 460;
     
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
     
-    var container = d3.select('body')
-    .append("svg") //put a new svg in the body
-   .attr("width", w) //assign the width
-   .attr("height", h) //assign the height
-   .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-   .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
-
-   //innerRect block
-   var innerRect = container.append("rect")
-   .datum(400) //a single value is a DATUM
-   .attr("width", function(d){ //rectangle width
-       return d * 2; //400 * 2 = 800
-   })
-   .attr("height", function(d){ //rectangle height
-       return d; //400
-   })
-   .attr("class", "innerRect") //class name
-   .attr("x", 50) //position from left on the x (horizontal) axis
-   .attr("y", 50) //position from top on the y (vertical) axis
-   .style("fill", "#FFFFFF"); //fill color
-
-   var dataArray = [10, 20, 30, 40, 50];
-   
-   //above Example 2.8 line 20
-   var x = d3.scaleLinear() //create the scale
-   .range([90, 750]) //output min and max
-   .domain([0, 3]); //input min and max
-   
-   //find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
-
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
-
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50])
-        .domain([0, 700000]);
+    //create Albers equal area conic projection centered on France
+    var projection = d3.geoAlbers()
+        .center([-90, 60])
+        .rotate([50, 40, -2])
+        .parallels([30, 49])
+        .scale(400)
+        .translate([width / 2, height / 2]);
     
-    //color scale generator 
-    var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
-   
-   
-   var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the index to place each circle horizontally
-            return x(i);
-        })
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
+    var path = d3.geoPath()
+        .projection(projection);
 
-    //create y axis generator
-    var yAxis = d3.axisLeft(y);
-    var axis = container.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(50, 0)")
-        .call(yAxis);
-    
-    //create a text element and add the title
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("City Populations");
-    
-    //create circle labels
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population);
-        });
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];    
+    promises.push(d3.csv("data/stateGDP.csv")); //load attributes from csv    
+    promises.push(d3.json("data/us_50state_4326.topojson")); //load background spatial data    
+    promises.push(d3.json("data/us_50state_4326.topojson")); //load background spatial data    
+    Promise.all(promises).then(callback);
 
-    var format = d3.format(",")
+    function callback(data){
+        csvData = data[0];    
+        basemap = data[1];
+        states = data[2];
+        //translate europe TopoJSON
+        var basemap = topojson.feature(basemap, basemap.objects.us_50state_4326);
+        var us_states = topojson.feature(states, states.objects.us_50state_4326).features;
+        // console.log(us_states)
 
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        });
+        var base = map.append("path")
+            .datum(basemap)
+            .attr("class", "states")
+            .attr("d", path)
 
-    //second line of label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population);
-        });
+        var states = map.selectAll(".regions")
+            .data(us_states)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "regions" + d.properties.name
+            })
+            .attr("d", path);
+
+        //create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
+
+         //Example 2.6 line 1...create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+        // //create graticule background
+        // var gratBackground = map.append("path")
+        //     .datum(graticule.outline()) //bind graticule background
+        //     .attr("class", "gratBackground") //assign class for styling
+        //     .attr("d", path) //project graticule
+
+        //Example 2.6 line 5...create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
+
+    };
 };
