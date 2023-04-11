fc78cb84fef492e0438ebc2678a6003e35eb8a21
kcgeo1234 <kcheng38@wisc.edu>
2023-04-11 15:31:48 -0500

    Activity 10 Update

--- js/main.js
+++ js/main.js
@@ -25,11 +25,8 @@
             .attr("height", height);
 
         //create Albers equal area conic projection centered on US
-        var projection = d3.geoAlbers()
-            .center([-90, 60])
-            .rotate([50, 40, -2])
-            .parallels([30, 49])
-            .scale(400)
+        var projection = d3.geoAlbersUsa()
+            .scale(800)
             .translate([width / 2, height / 2]);
 
         var path = d3.geoPath()
@@ -39,16 +36,12 @@
         var promises = [];    
         promises.push(d3.csv("data/stateGDP_perC.csv")); //load attributes from csv    
         promises.push(d3.json("data/us_50state_4326.topojson")); //load background spatial data    
-        promises.push(d3.json("data/us_50state_4326.topojson")); //load background spatial data    
         Promise.all(promises).then(callback);
 
         function callback(data){
             csvData = data[0],    
             basemap = data[1],
-            states = data[2];
-
-            setGraticule(map, path);
-
+            states = data[1];
 
             //translate TopoJSON
             var basemap = topojson.feature(basemap, basemap.objects.us_50state_4326);
@@ -56,7 +49,7 @@
 
             var base = map.append("path")
                 .datum(basemap)
-                .attr("class", "states")
+                .attr("class", "basemap")
                 .attr("d", path);
             
             var colorScale = makeColorScale(csvData);
@@ -108,7 +101,8 @@
         .enter()
         .append("rect")
         .sort(function(a, b){
-            return b[expressed]-a[expressed]
+            console.log(a);
+            return a[expressed]-b[expressed]
         })
         .attr("class", function(d){
             return "bar " + d.name;
@@ -158,14 +152,6 @@
         //create graticule generator
         var graticule = d3.geoGraticule()
         .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
-
-        //create graticule lines
-        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines
 
         // //create graticule background
         // var gratBackground = map.append("path")
